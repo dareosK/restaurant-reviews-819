b6cb227775cb4e06737ffd64a4b6f51a34a97fce
Dareos Khalili <khalilidareos@gmail.com>
2022-02-16 11:29:47 +0100

    finishes the app

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,15 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
+  def top
+    # return the top restaurants from my db
+    @restaurants = Restaurant.where(stars: 5)
+  end
+
+  def chef
+    @restaurant = Restaurant.find(params[:id])
+    @chef_name = @restaurant.chef_name
+  end
   # GET /restaurants
   def index
     @restaurants = Restaurant.all
